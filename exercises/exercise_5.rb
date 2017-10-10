bd250b676800fed6cdf87e0870501f8fb8339909
Brandon Day <brandontday7@gmail.com>
2017-10-10 16:25:43 +0000

    did the first 5 exercises

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,12 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "$#{total_revenue}"
+
+num_of_locations = Store.count
+
+puts "Average annual revenue: $#{total_revenue/num_of_locations}"
+
+num_of_millionaires = Store.where("annual_revenue > 1000000").count
+puts "Number of stores over $1 000 000: #{num_of_millionaires}"
